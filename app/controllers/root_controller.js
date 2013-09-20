f4bf242f4503ed71a81cf527d414339d3130dffa
Ryan C. Creasey <rcreasey@blizzard.com>
2013-09-20 12:40:16 -0700

    Cleaning up Root view.

--- app/controllers/root_controller.js
+++ app/controllers/root_controller.js
@@ -1,10 +1,13 @@
 var locomotive = require('locomotive')
   , Controller = locomotive.Controller
   , RootController = new Controller()
-
+  , InvType = require('../models/invType')
 
 RootController.index = function(req, res) {
-  res.render('root/index', {title: 'Market', user: req.user});
+  var self = this;
+  InvType.find({}).sort('typeName').exec(function(err, items) {
+    res.render('root/index', { title: 'Market', user: req.user, items: items });
+  });
 };
 
 module.exports = RootController;
--- tasks/goonmetrics.js
+++ tasks/goonmetrics.js
@@ -69,7 +69,6 @@ module.exports = function(grunt) {
  
           }
         ], function() {
-
           callback();
         });
 
